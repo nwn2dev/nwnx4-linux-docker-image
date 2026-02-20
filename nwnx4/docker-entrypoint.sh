e016e4790ad2b0fcfc5a27098fa9be7c2b61b6e5
Scott Munday <scottmunday84@gmail.com>
2026-02-20 17:32:28 -0500

    Fixed issues; a little bulkier, but any DLLs and EXEs will not work well between Docker to volume to wine

--- nwnx4/docker-entrypoint.sh
+++ nwnx4/docker-entrypoint.sh
@@ -2,34 +2,22 @@
 
 set -euxo pipefail
 
-# If the NWN2 stage file does not exist in NWN2 install directory, drop
-for file in $(ls /opt/nwn2-stage); do
-  if [[ ! -e "/srv/nwn2/$file" ]]; then
-    rm -f "/opt/nwn2-stage/$file"
+# Sync NWN2 install directory to NWN2 stage directory
+if [[ ! -e "/opt/nwn2-stage/.nwn2-staged" ]]; then
+  rsync -avz --chown=nwnx4:nwnx4 --ignore-existing /srv/nwn2/ /opt/nwn2-stage/
+  rm -rf /opt/nwn2-stage/Miles
+  touch /opt/nwn2-stage/.nwn2-staged
+fi
+
+# Create NWNX4 user directory symlinks to NWNX4 install directory
+for file in $(ls /srv/nwnx4-user); do
+  if [[ ! -e "/opt/nwnx4/$file" ]]; then
+    ln -s "/srv/nwnx4-user/$file" /opt/nwnx4/ && chown nwnx4:nwnx4 -h "/opt/nwnx4/$file"
   fi
 done
 
-# If the NWN2 install file does not exist in NWN2 stage directory, add
-for file in $(ls /srv/nwn2); do
-  if [[ ! -e "/opt/nwn2-stage/$file" ]]; then
-    ln -s "/srv/nwn2/$file" /opt/nwn2-stage/ && chown nwnx4:nwnx4 -h "/opt/nwn2-stage/$file"
-  fi
-done
-
-# Clear all files in plugin folder; do this every startup
-rm -Rf /etc/nwnx4/plugins/*;
-
-# Copy a plugin if it exists; prefer /srv/nwnx4-user plugins
-for file in $(ls /srv/nwnx4-user/plugins/*.dll | xargs -n 1 basename); do
-  cp "/srv/nwnx4-user/plugins/$file" /etc/nwnx4/plugins/ && chown nwnx4:nwnx4 "/etc/nwnx4/plugins/$file"
-done
-
-# Copy a plugin if it exists; only add /opt/nwnx4 plugins if it doesn't exist in /srv/nwnx4-user
-for file in $(ls /opt/nwnx4/plugins/*.dll | xargs -n 1 basename); do
-  if [[ ! -e "/etc/nwnx4/plugins/$file" ]]; then
-    cp "/opt/nwnx4/plugins/$file" /etc/nwnx4/plugins/ && chown nwnx4:nwnx4 "/etc/nwnx4/plugins/$file"
-  fi
-done
+# Copy plugins from NWNX4 user directory to NWNX4 install directory
+rsync -avz --chown=nwnx4:nwnx4 --ignore-existing /srv/nwnx4-user/plugins/*.dll /opt/nwnx4/plugins/
 
 # Wine doesn't support NCrypt well; building it here through openssl
 CERTIFICATE_PATH="/srv/nwnx4-user/NWNCertificate"
@@ -64,9 +52,8 @@ fi
 chown -R nwnx4:nwnx4 /srv/nwnx4-user
 chown -R nwnx4:nwnx4 /srv/nwn2-logs
 
-# Setup Xvfb; execute command
 gosu nwnx4 bash <<-EOF
-  Xvfb $DISPLAY -screen 0 1024x768x16 &
+  Xvfb $DISPLAY &
   wine reg import /opt/nwn2.reg
 EOF
 
